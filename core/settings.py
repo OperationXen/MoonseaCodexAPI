b42300f3f039e493c505c12b5bc6b7d8285737b4
OperationXen <OperationXen@apocryphasecurity.com>
2022-05-14 12:08:09 +0100

    Remove CSRF from API endpoints

--- core/settings.py
+++ core/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
+        'codex.utils.security.SessionCSRFExemptAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
