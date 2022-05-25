b85b183d4cb4716bf0e370b5bcca5bd67c7aa1e8
OperationXen <operationxen@apocryphasecurity.com>
2022-05-25 21:08:11 +0100

    Moved to simpler item representation (#5)
    * Moved to simpler item representation
    
    * Moved to workflows
    
    * fixed formatting
    
    * Added setup python path
    
    * Changed names

--- codex/serialisers/data.py
+++ codex/serialisers/data.py
@@ -5,71 +5,44 @@ from codex.models.items import MagicItem
 
 
 class MagicItemSerialiser(serializers.ModelSerializer):
-    """ Serialiser for a magic item """
-    owner = serializers.ReadOnlyField(source='character.name', read_only=True)
+    """Serialiser for a magic item"""
+
+    owner = serializers.ReadOnlyField(source="character.name", read_only=True)
 
     class Meta:
         model = MagicItem
-        fields = ['id', 'owner', 'name', 'rarity']
+        fields = ["id", "owner", "name", "rarity", "equipped"]
 
 
 class MagicItemSummarySerialiser(serializers.ModelSerializer):
-    """ Serialiser for a magic item """
+    """Serialiser for a magic item"""
+
     class Meta:
         model = MagicItem
-        fields = ['id', 'rarity', 'name']
+        fields = ["id", "rarity", "name"]
 
 
 class MagicItemCreationSerialiser(serializers.ModelSerializer):
-    """ All fields of magic item for creation operations """
+    """All fields of magic item for creation operations"""
+
     class Meta:
         model = MagicItem
-        fields = '__all__'
-
-
-class MagicItemEquippedListSerialiser(serializers.ListSerializer):
-    def to_representation(self, data):
-        """ Override this function to apply the filter before passing the data back """
-        data = data.filter(equipped=True)
-        return super(MagicItemEquippedListSerialiser, self).to_representation(data)
-
-
-class CharacterEquippedMagicItemSerialiser(serializers.ModelSerializer):
-    """ Serialiser that filters items to only those currently equipped """
-    
-    class Meta:
-        list_serializer_class = MagicItemEquippedListSerialiser
-        model = MagicItem
-        fields = '__all__'
-
-
-class MagicItemAdditionalListSerialiser(serializers.ListSerializer):
-    def to_representation(self, data):
-        """ Override this function to apply the filter before passing the data back """
-        data = data.filter(equipped=False)
-        return super(MagicItemAdditionalListSerialiser, self).to_representation(data)
-
-
-class CharacterAdditionalMagicItemSerialser(serializers.ModelSerializer):
-    class Meta:
-        list_serializer_class = MagicItemAdditionalListSerialiser
-        model = MagicItem
-        fields = '__all__'
+        fields = "__all__"
 
 
 class CharacterDetailsSerialiser(serializers.ModelSerializer):
-    """ Serialiser to use for individual player characters, includes inventories """
-    equipped_items = CharacterEquippedMagicItemSerialiser(many=True, source='magicitems')
-    additional_items = CharacterAdditionalMagicItemSerialser(many=True, source='magicitems')
-    
+    """Serialiser to use for individual player characters, includes inventories"""
+
+    items = MagicItemSerialiser(many=True, source="magicitems")
+
     class Meta:
         model = Character
-        exclude = ['player', 'public']
-        additional_fields = ['equipped_items', 'additional_items']
+        exclude = ["player", "public"]
+
 
 class CharacterSerialiser(serializers.ModelSerializer):
-    """ Serialiser to use for creating or updating player characters """
+    """Serialiser to use for creating or updating player characters"""
 
     class Meta:
         model = Character
-        exclude = ['player']
+        exclude = ["player"]
--- codex/tests/views/data/test_views_data_character.py
+++ codex/tests/views/data/test_views_data_character.py
@@ -9,8 +9,9 @@ from codex.models.character import Character
 
 
 class TestCharacterCRUDViews(TestCase):
-    """ Check character create / retrieve / update / delete functionality """
-    fixtures = ['test_users', 'test_characters', 'test_magicitems']
+    """Check character create / retrieve / update / delete functionality"""
+
+    fixtures = ["test_users", "test_characters", "test_magicitems"]
 
     valid_data = {
         "player": 2,
@@ -29,127 +30,124 @@ class TestCharacterCRUDViews(TestCase):
         "dc": 12,
         "vision": "Darkvision (60ft)",
         "biography": "Meeka has big pet called Scritch. Meeka and Scritch go on adventures.",
-        "dm_text": "Pack tactics!"
+        "dm_text": "Pack tactics!",
     }
 
     def test_anonymous_user_cannot_create_character(self) -> None:
-        """ A user who isn't logged in should be prevented from creating a character """
+        """A user who isn't logged in should be prevented from creating a character"""
         test_data = copy(self.valid_data)
 
-        response = self.client.post(reverse('character-list'), test_data)
+        response = self.client.post(reverse("character-list"), test_data)
         self.assertEqual(response.status_code, HTTP_403_FORBIDDEN)
         with self.assertRaises(Character.DoesNotExist):
-            character = Character.objects.get(name=test_data['name'])
+            character = Character.objects.get(name=test_data["name"])
 
     def test_authenticated_user_can_create_character(self) -> None:
-        """ Ensure that a correctly authed user can create a character """
+        """Ensure that a correctly authed user can create a character"""
         test_data = copy(self.valid_data)
 
-        self.client.login(username='testuser1', password='testpassword')
-        response = self.client.post(reverse('character-list'), test_data)
+        self.client.login(username="testuser1", password="testpassword")
+        response = self.client.post(reverse("character-list"), test_data)
         self.assertEqual(response.status_code, HTTP_201_CREATED)
-        
-        character = Character.objects.get(name=test_data['name'])
-        self.assertEqual(response.data['equipped_items'], [])
+
+        character = Character.objects.get(name=test_data["name"])
+        self.assertEqual(response.data["items"], [])
         self.assertEqual(character.player, response.wsgi_request.user)
-        self.assertEqual(character.biography, test_data['biography'])
-        self.assertEqual(character.vision, test_data['vision'])
-    
+        self.assertEqual(character.biography, test_data["biography"])
+        self.assertEqual(character.vision, test_data["vision"])
+
     def test_anonymous_user_can_retrieve_character(self) -> None:
-        """ Anonymous users can read data, including specific characters """
-        response = self.client.get(reverse('character-detail', kwargs={'pk': '1'}))
+        """Anonymous users can read data, including specific characters"""
+        response = self.client.get(reverse("character-detail", kwargs={"pk": "1"}))
         self.assertEqual(response.status_code, HTTP_200_OK)
 
     def test_player_information_not_disclosed_on_character(self) -> None:
-        """ Player character ownership information should be hidden from users for privacy reasons """
-        self.client.login(username='testuser1', password='testpassword')
+        """Player character ownership information should be hidden from users for privacy reasons"""
+        self.client.login(username="testuser1", password="testpassword")
 
-        response = self.client.get(reverse('character-detail', kwargs={'pk': '1'}))
+        response = self.client.get(reverse("character-detail", kwargs={"pk": "1"}))
         self.assertEqual(response.status_code, HTTP_200_OK)
-        self.assertNotIn('player', response.data)
+        self.assertNotIn("player", response.data)
 
     def test_magicitems_in_character(self) -> None:
-        """ Ensure that magic items are included when a character is fetched """
-        self.client.login(username='testuser1', password='testpassword')
+        """Ensure that magic items are included when a character is fetched"""
+        self.client.login(username="testuser1", password="testpassword")
 
-        response = self.client.get(reverse('character-detail', kwargs={'pk': '1'}))
+        response = self.client.get(reverse("character-detail", kwargs={"pk": "1"}))
         self.assertEqual(response.status_code, HTTP_200_OK)
-        self.assertIn('equipped_items', response.data)
-        self.assertGreaterEqual(len(response.data['equipped_items']), 3)
+        self.assertIn("items", response.data)
+        self.assertGreaterEqual(len(response.data["items"]), 3)
 
     def test_anonymous_user_can_list_characters(self) -> None:
-        response = self.client.get(reverse('character-list'))
+        response = self.client.get(reverse("character-list"))
         self.assertEqual(response.status_code, HTTP_200_OK)
-        self.assertEqual(response.data['count'], 2)
-        self.assertEqual(response.data['results'][0]['name'], 'Meepo')
-        self.assertEqual(response.data['results'][1]['name'], 'Meela')
+        self.assertEqual(response.data["count"], 2)
+        self.assertEqual(response.data["results"][0]["name"], "Meepo")
+        self.assertEqual(response.data["results"][1]["name"], "Meela")
 
     def test_anonymous_user_cannot_update_characters(self) -> None:
-        """ A user who isn't logged in shouldn't be allowed to update characters """
-        test_data = {
-            "classes": "Paladin (Vengeance) 10",
-            "gold": 42.0
-        }
-        response = self.client.patch(reverse('character-detail', kwargs={'pk': '2'}), json.dumps(test_data), content_type='application/json')
+        """A user who isn't logged in shouldn't be allowed to update characters"""
+        test_data = {"classes": "Paladin (Vengeance) 10", "gold": 42.0}
+        response = self.client.patch(
+            reverse("character-detail", kwargs={"pk": "2"}), json.dumps(test_data), content_type="application/json"
+        )
         self.assertEqual(response.status_code, HTTP_403_FORBIDDEN)
-    
-    def test_cannot_update_other_users_characters(self) -> None:
-        """ A user should not be able to update other peoples characters """
-        self.client.login(username='testuser2', password='testpassword')
 
-        test_data = {
-            "classes": "Paladin (Vengeance) 10",
-            "gold": 42.0
-        }
-        response = self.client.patch(reverse('character-detail', kwargs={'pk': '2'}), json.dumps(test_data), content_type='application/json')
+    def test_cannot_update_other_users_characters(self) -> None:
+        """A user should not be able to update other peoples characters"""
+        self.client.login(username="testuser2", password="testpassword")
+
+        test_data = {"classes": "Paladin (Vengeance) 10", "gold": 42.0}
+        response = self.client.patch(
+            reverse("character-detail", kwargs={"pk": "2"}), json.dumps(test_data), content_type="application/json"
+        )
         self.assertEqual(response.status_code, HTTP_403_FORBIDDEN)
 
     def test_user_can_update_character(self) -> None:
-        """ A user should be able to update their own characters """
-        self.client.login(username='testuser1', password='testpassword')
+        """A user should be able to update their own characters"""
+        self.client.login(username="testuser1", password="testpassword")
 
-        test_data = {
-            "classes": "Paladin (Vengeance) 10",
-            "gold": 42.0
-        }
-        response = self.client.patch(reverse('character-detail', kwargs={'pk': '2'}), json.dumps(test_data), content_type='application/json')
+        test_data = {"classes": "Paladin (Vengeance) 10", "gold": 42.0}
+        response = self.client.patch(
+            reverse("character-detail", kwargs={"pk": "2"}), json.dumps(test_data), content_type="application/json"
+        )
         self.assertEqual(response.status_code, HTTP_200_OK)
         self.assertGreater(len(response.data), 10)
-        self.assertIn('name', response.data)
-        self.assertEquals('Meela', response.data['name'])
+        self.assertIn("name", response.data)
+        self.assertEqual("Meela", response.data["name"])
 
         character = Character.objects.get(pk=2)
-        self.assertEqual(character.classes, test_data['classes'])
-        self.assertEqual(character.gold, test_data['gold'])
+        self.assertEqual(character.classes, test_data["classes"])
+        self.assertEqual(character.gold, test_data["gold"])
         self.assertEqual(character.name, "Meela")
-    
+
     def test_anonymous_user_cannot_delete_characters(self) -> None:
-        """ Prevent a user who isn't logged in from deleting characters """
-        response = self.client.delete(reverse('character-detail', kwargs={'pk': '1'}))
+        """Prevent a user who isn't logged in from deleting characters"""
+        response = self.client.delete(reverse("character-detail", kwargs={"pk": "1"}))
         self.assertEqual(response.status_code, HTTP_403_FORBIDDEN)
         try:
             character = Character.objects.get(id=1)
         except Character.DoesNotExist:
-            self.fail('character object should not have been deleted')
+            self.fail("character object should not have been deleted")
 
     def test_cannot_delete_other_users_characters(self) -> None:
-        """ A user must be prevented from deleting character that belong to other users """
-        self.client.login(username='testuser2', password='testpassword')
+        """A user must be prevented from deleting character that belong to other users"""
+        self.client.login(username="testuser2", password="testpassword")
 
-        response = self.client.delete(reverse('character-detail', kwargs={'pk': '1'}))
+        response = self.client.delete(reverse("character-detail", kwargs={"pk": "1"}))
         self.assertEqual(response.status_code, HTTP_403_FORBIDDEN)
         try:
             character = Character.objects.get(id=1)
         except Character.DoesNotExist:
-            self.fail('character object should not have been deleted')
+            self.fail("character object should not have been deleted")
 
     def test_can_delete_character(self) -> None:
-        """ A user can delete their own character """
-        self.client.login(username='testuser1', password='testpassword')
+        """A user can delete their own character"""
+        self.client.login(username="testuser1", password="testpassword")
 
-        response = self.client.delete(reverse('character-detail', kwargs={'pk': '1'}))
+        response = self.client.delete(reverse("character-detail", kwargs={"pk": "1"}))
         self.assertEqual(response.status_code, HTTP_200_OK)
-        self.assertIn('message', response.data)
-        self.assertIn("Character destroyed", response.data['message'])
+        self.assertIn("message", response.data)
+        self.assertIn("Character destroyed", response.data["message"])
         with self.assertRaises(Character.DoesNotExist):
             Character.objects.get(id=1)
