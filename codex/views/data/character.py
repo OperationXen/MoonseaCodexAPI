eb4546b5398c2575218fdd63b7c10bc095a91151
OperationXen <OperationXen@apocryphasecurity.com>
2022-05-25 20:32:39 +0100

    Moved to simpler item representation

--- codex/views/data/character.py
+++ codex/views/data/character.py
@@ -8,50 +8,51 @@ from codex.serialisers.data import CharacterSerialiser, CharacterDetailsSerialis
 
 
 class CharacterViewSet(viewsets.GenericViewSet):
-    """ CRUD views for character model """
+    """CRUD views for character model"""
+
     serializer_class = CharacterSerialiser
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
-        """ Retrieve base queryset """
+        """Retrieve base queryset"""
         return Character.objects.filter(public=True)
-    
+
     def create(self, request):
-        """ Create a new character, ownership set to requesting user """
+        """Create a new character, ownership set to requesting user"""
         serialiser = CharacterSerialiser(data=request.data)
         if serialiser.is_valid():
             character = serialiser.save(player=request.user)
             new_character = CharacterDetailsSerialiser(character)
             return Response(new_character.data, HTTP_201_CREATED)
         else:
-            return Response({'message': 'Character creation failed'}, HTTP_400_BAD_REQUEST)
+            return Response({"message": "Character creation failed"}, HTTP_400_BAD_REQUEST)
 
     def retrieve(self, response, *args, **kwargs):
-        """ Get details for a single character """
+        """Get details for a single character"""
         character = self.get_object()
         serializer = CharacterDetailsSerialiser(character)
         return Response(serializer.data)
 
     def list(self, request):
-        """ List all characters (paginated) """
+        """List all characters (paginated)"""
         serialiser = CharacterDetailsSerialiser(self.get_queryset(), many=True)
         return self.get_paginated_response(self.paginate_queryset(serialiser.data))
 
     def partial_update(self, request, *args, **kwargs):
-        """ Allow a user to update their characters """
+        """Allow a user to update their characters"""
         existing_character = self.get_object()
         if existing_character.player != request.user:
-            return Response({'message': 'This character does not belong to you'}, HTTP_403_FORBIDDEN)
+            return Response({"message": "This character does not belong to you"}, HTTP_403_FORBIDDEN)
         serialiser = CharacterSerialiser(existing_character, data=request.data, partial=True)
         if serialiser.is_valid():
             character = serialiser.save()
             return Response(serialiser.data, HTTP_200_OK)
         else:
-            return Response({'message': 'Invalid data in character update'}, HTTP_400_BAD_REQUEST)
+            return Response({"message": "Invalid data in character update"}, HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, *args, **kwargs):
         character = self.get_object()
         if character.player != request.user:
-            return Response({'message': 'This character does not belong to you'}, HTTP_403_FORBIDDEN)
+            return Response({"message": "This character does not belong to you"}, HTTP_403_FORBIDDEN)
         character.delete()
-        return Response({'message': 'Character destroyed'}, HTTP_200_OK)
+        return Response({"message": "Character destroyed"}, HTTP_200_OK)
