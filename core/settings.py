11121ee2b83904acb99dc539d39b1aef875e7ebb
OperationXen <OperationXen@apocryphasecurity.com>
2022-05-27 17:38:10 +0100

    Moved admin page static files to prevent clash

--- core/settings.py
+++ core/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "admin_static/"
 STATIC_ROOT = BASE_DIR / 'static_files'
 
 # Default primary key field type
