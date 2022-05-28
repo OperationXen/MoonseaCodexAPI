dd150ce3822d872109f626891aacdbf1c45b3692
OperationXen <operationxen@apocryphasecurity.com>
2022-05-28 17:26:23 +0100

    Changed static file name (#21)

--- core/settings.py
+++ core/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "admin_static/"
-STATIC_ROOT = BASE_DIR / 'static_files'
+STATIC_ROOT = BASE_DIR / 'admin_static'
 
 MEDIA_ROOT = BASE_DIR / 'media/'
 
