4b901c97cc907c8b247099143d8fb3f30bc4667e
OperationXen <OperationXen@apocryphasecurity.com>
2022-05-28 17:05:45 +0100

    Added media folder

--- core/settings.py
+++ core/settings.py
@@ -126,6 +126,8 @@ USE_TZ = True
 STATIC_URL = "admin_static/"
 STATIC_ROOT = BASE_DIR / 'static_files'
 
+MEDIA_ROOT = BASE_DIR / 'media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
