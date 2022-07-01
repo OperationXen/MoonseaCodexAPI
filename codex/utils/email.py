542dcfe8671f37fb1fe9674383dec879c716e4bf
OperationXen <operationxen@apocryphasecurity.com>
2022-07-01 11:47:44 +0100

    Pulling server name from config (#64)
    * Pulling server name from config
    
    * Fixed syntax fault

--- codex/utils/email.py
+++ codex/utils/email.py
@@ -1,17 +1,16 @@
 from django.template.loader import render_to_string
-from django.contrib.sites.shortcuts import get_current_site
+from core.settings import SERVER
 
 
 def send_account_confirm_email(request, user, activation_token):
     """Generate a confirmatin token and send it to the user"""
     subject = "Welcome to the Moonsea Codex"
-    current_site = get_current_site(request)
     message = render_to_string(
         "emails/account_activation_email.html",
         {
             "user": user,
             "user_id": user.pk,
-            "domain": current_site.domain,
+            "domain": SERVER,
             "token": activation_token,
         },
     )
