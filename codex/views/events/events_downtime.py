8e55393e6d765422f68a74351330cb5547a433b1
OperationXen <OperationXen@apocryphasecurity.com>
2022-09-19 14:09:52 +0100

    Added tests and converted view to generic view

--- codex/views/events/events_downtime.py
+++ codex/views/events/events_downtime.py
@@ -1,7 +1,6 @@
-from nis import cat
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.status import *
 from codex.models.character import Character
 
@@ -9,77 +8,80 @@ from codex.models.events_downtime import CatchingUp
 from codex.serialisers.events_downtime import CatchingUpSerialiser
 
 
-class EventDowntimeCatchingUpView(APIView):
+class EventDowntimeCatchingUpView(viewsets.GenericViewSet):
     """CRUD views for catching up events"""
+    lookup_field = "uuid"
+    lookup_url_kwarg = "uuid"
+    lookup_value_regex = "[\-0-9a-f]{36}"
 
+    serializer_class = CatchingUpSerialiser
     permission_classes = [IsAuthenticatedOrReadOnly]
 
-    def lookup_character(request):
-        """Lookup an existing character, returning a suitable response if not found"""
-        try:
-            character_uuid = request.data.get("character_uuid")
-            if not character_uuid:
-                return Response({"message": "No character specified"}, HTTP_400_BAD_REQUEST)
+    def get_queryset(self):
+        """ Base queryset for building on"""
+        return CatchingUp.objects.all()
 
-            character = Character.objects.get(uuid=character_uuid)
-            if character.player is not request.user:
-                raise PermissionError()
-            return character
+    def lookup_character(self, request):
+        """Lookup an existing character, returning a suitable error if not found"""
+        character_uuid = request.data.get("character_uuid")
+        if not character_uuid:
+            raise ValueError()
+        character = Character.objects.get(uuid=character_uuid)
+        if character.player != request.user:
+            raise PermissionError()
+        return character
+
+    def create(self, request):
+        """Create a new CatchingUp event"""
+        try:
+            char = self.lookup_character(request)
         except Character.DoesNotExist as e:
             return Response({"message": "Unable to find matching character"}, HTTP_400_BAD_REQUEST)
         except PermissionError as pe:
             return Response({"message": "This character does not belong to you"}, HTTP_403_FORBIDDEN)
+        except ValueError as ve:
+            return Response({"message": "No character specified"}, HTTP_400_BAD_REQUEST)
 
-    def get(self, request, event_uuid):
+        serialiser = CatchingUpSerialiser(data = request.data)
+        if serialiser.is_valid():
+            event = serialiser.save(character=char)
+            return Response(serialiser.data, HTTP_201_CREATED)
+        else:
+            return Response({'message': 'Invalid request data'}, HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, response, *args, **kwargs):
         """Retrieve a specific catchingup event"""
-        try:
-            event = CatchingUp.objects.get(uuid=event_uuid)
-        except CatchingUp.DoesNotExist:
-            return Response({"message": "Unable to find item"}, HTTP_400_BAD_REQUEST)
-
+        event = self.get_object()
         serialiser = CatchingUpSerialiser(event)
-        return Response(serialiser.data, HTTP_200_OK)
+        return Response(serialiser.data)
 
-    def post(self, request):
-        """Create a new CatchingUp event"""
-        data = self.lookup_character(request)
-        if type(data) == Response:
-            return data
-
-        catchingup = CatchingUp(character=data)
-        catchingup_serialiser = CatchingUpSerialiser(catchingup)
-        return Response(catchingup_serialiser.data, HTTP_200_OK)
-
-    def patch(self, request, event_uuid):
+    def partial_update(self, request, *args, **kwargs):
         """Update an existing CatchingUp event"""
-        catchingup_uuid = request.data.get("uuid")
+        event = self.get_object()
         try:
-            catchingup = CatchingUp.objects.get(uuid=catchingup_uuid)
-            if catchingup.character.player is not request.user:
+            if event.character.player is not request.user:
                 raise PermissionError
-
-            serialiser = CatchingUpSerialiser(catchingup, request.data, partial=True)
+            serialiser = CatchingUpSerialiser(event, request.data, partial=True)
             if serialiser.is_valid():
                 serialiser.save()
-                return Response({"message": "Event deleted"}, HTTP_200_OK)
+                return Response({"message": "Event updated"}, HTTP_200_OK)
             return Response({"message": "Invalid update data"}, HTTP_400_BAD_REQUEST)
 
         except CatchingUp.DoesNotExist as e:
-            return Response({"message": "This catching up event doesnt exist"}, HTTP_400_BAD_REQUEST)
+            return Response({"message": "This event doesnt exist"}, HTTP_400_BAD_REQUEST)
         except PermissionError as pe:
-            return Response({"message": "This catching up event doesnt belong to you"}, HTTP_403_FORBIDDEN)
+            return Response({"message": "This event doesnt belong to you"}, HTTP_403_FORBIDDEN)
 
-    def delete(self, request):
+    def destroy(self, request, *args, **kwargs):
         """Delete an existing CatchingUp event"""
-        catchingup_uuid = request.data.get("uuid")
         try:
-            catchingup = CatchingUp.objects.get(uuid=catchingup_uuid)
-            if catchingup.character.player is not request.user:
+            event = self.get_object()
+            if event.character.player != request.user:
                 raise PermissionError
-            catchingup.delete()
+            event.delete()
             return Response({"message": "Event deleted"}, HTTP_200_OK)
 
         except CatchingUp.DoesNotExist as e:
-            return Response({"message": "This catching up event doesnt exist"}, HTTP_400_BAD_REQUEST)
+            return Response({"message": "This event doesnt exist"}, HTTP_400_BAD_REQUEST)
         except PermissionError as pe:
-            return Response({"message": "This catching up event doesnt belong to you"}, HTTP_403_FORBIDDEN)
+            return Response({"message": "This character doesnt belong to you"}, HTTP_403_FORBIDDEN)
