a5e8e887f47665ba003f9932cf6936c3b3ce62ac
OperationXen <OperationXen@apocryphasecurity.com>
2022-09-19 21:16:28 +0100

    Added model for mundane trade

--- codex/serialisers/events_downtime.py
+++ codex/serialisers/events_downtime.py
@@ -5,9 +5,21 @@ from codex.models.events_downtime import CatchingUp
 
 class CatchingUpSerialiser(serializers.ModelSerializer):
     """Serialiser for a catching up event"""
+
     event_type = serializers.ReadOnlyField(default="dt_catchingup")
 
     class Meta:
         model = CatchingUp
         fields = ["uuid", "datetime", "character", "levels", "details", "event_type"]
-        read_only_fields = ['character']
+        read_only_fields = ["character"]
+
+
+class MundaneTradeSerialiser(serializers.ModelSerializer):
+    """Serialiser for a mundane trade event"""
+
+    event_type = serializers.ReadOnlyField(default="dt_mtrade")
+
+    class Meta:
+        model = CatchingUp
+        fields = ["uuid", "datetime", "character", "gold_change", "sold", "purchased", "event_type"]
+        read_only_fields = ["character"]
--- codex/tests/models/test_models_event_downtime.py
+++ codex/tests/models/test_models_event_downtime.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 from codex.models.character import Character
-from codex.models.events_downtime import CatchingUp
+from codex.models.events_downtime import CatchingUp, MundaneTrade
 
 
 class TestCatchUpModel(TestCase):
@@ -31,6 +31,34 @@ class TestCatchUpModel(TestCase):
     def test_text_representation(self) -> None:
         """Check that a CatchingUp event can be represented as a string"""
         obj = CatchingUp.objects.get(pk=1)
+        str_rep = str(obj)
+
+        self.assertIn(obj.character.name, str_rep)
+
+
+class TestMundaneTrade(TestCase):
+    """Tests for the mundate trade"""
+
+    fixtures = ["test_users", "test_characters", "test_events_mundanetrade"]
+
+    def test_create(self):
+        """Test that an instance of the model can be created"""
+        char = Character.objects.get(pk=1)
+        obj = MundaneTrade(character=char, purchased="Potion of healing", gold_change=50)
+        self.assertIsInstance(obj, MundaneTrade)
+
+    def test_delete(self):
+        """Test that you can delete a trade event"""
+        obj = MundaneTrade.objects.get(pk=1)
+        self.assertIsInstance(obj, MundaneTrade)
+        try:
+            obj.delete()
+        except:
+            self.fail("Unable to delete mundanetrade event")
+
+    def test_text_represenation(self):
+        """Test that the object can be converted to a text representation"""
+        obj = MundaneTrade.objects.get(pk=1)
         str_rep = str(obj)
 
         self.assertIn(obj.character.name, str_rep)
