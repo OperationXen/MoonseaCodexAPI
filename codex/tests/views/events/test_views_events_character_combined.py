747b21b369ace06a57103a3e67446a19d45d7fd3
OperationXen <OperationXen@apocryphasecurity.com>
2022-09-22 13:23:35 +0100

    Added mundane trade events to results

--- codex/tests/views/events/test_views_events_character_combined.py
+++ codex/tests/views/events/test_views_events_character_combined.py
@@ -4,28 +4,66 @@ from django.urls import reverse
 
 from codex.models.character import Character
 
+
 class TestCharacterEventView(TestCase):
-    """ Check character_events list query """
-    fixtures = ["test_users", "test_characters", "test_character_games"]
-        
+    """Check character_events list query"""
+
+    fixtures = [
+        "test_users",
+        "test_characters",
+        "test_character_games",
+        "test_events_dt_mundanetrade",
+        "test_events_dt_catchingup",
+    ]
+
     def test_anonymous_user_query_by_uuid(self) -> None:
-        """ Unauthenticated user can query by character UUID """
+        """Unauthenticated user can query by character UUID"""
         self.client.logout()
         character = Character.objects.get(pk=2)
 
-        response = self.client.get(reverse("character_events", kwargs={'character_uuid': character.uuid}))
+        response = self.client.get(reverse("character_events", kwargs={"character_uuid": character.uuid}))
         self.assertEqual(response.status_code, HTTP_200_OK)
         self.assertIsInstance(response.data, list)
         self.assertEqual(len(response.data), 3)
-        self.assertIn('event_type', response.data[0])
-        self.assertEqual(response.data[0].get('event_type'), 'game')
+        self.assertIn("event_type", response.data[0])
+        self.assertEqual(response.data[0].get("event_type"), "game")
 
     def test_anonymous_user_missing_uuid(self) -> None:
-        """ An anonymous user should get an error if they don't specify a UUID """
+        """An anonymous user should get an error if they don't specify a UUID"""
         response = self.client.get("/api/character_events/")
         self.assertEqual(response.status_code, HTTP_404_NOT_FOUND)
 
     def test_anonymous_user_bad_uuid(self) -> None:
-        """ User should be given an error if they specify an invalid UUID """
-        response = self.client.get(reverse("character_events", kwargs={'character_uuid': "12345678-1234-1234-1234-12345678abcd"}))
+        """User should be given an error if they specify an invalid UUID"""
+        response = self.client.get(
+            reverse("character_events", kwargs={"character_uuid": "12345678-1234-1234-1234-12345678abcd"})
+        )
         self.assertEqual(response.status_code, HTTP_404_NOT_FOUND)
+
+    def test_downtime_activity_catchingup(self) -> None:
+        """Ensure that the catching up activity is correctly represented"""
+        self.client.logout()
+        character = Character.objects.get(pk=1)
+
+        response = self.client.get(reverse("character_events", kwargs={"character_uuid": character.uuid}))
+        self.assertEqual(response.status_code, HTTP_200_OK)
+        events = response.data
+        self.assertIsInstance(events, list)
+        for event in events:
+            if event["event_type"] == "dt_catchingup":
+                return True
+        self.fail("Expected catching up event not returned")
+
+    def test_downtime_activity_mundanetrade(self) -> None:
+        """Ensure that the mundane trade activity is correctly represented"""
+        self.client.logout()
+        character = Character.objects.get(pk=1)
+
+        response = self.client.get(reverse("character_events", kwargs={"character_uuid": character.uuid}))
+        self.assertEqual(response.status_code, HTTP_200_OK)
+        events = response.data
+        self.assertIsInstance(events, list)
+        for event in events:
+            if event["event_type"] == "dt_mtrade":
+                return True
+        self.fail("Expected mundane trade event not returned")
