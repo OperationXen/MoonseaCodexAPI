c771274928e8db4c9a92a5ed591a8e7ff9589542
OperationXen <OperationXen@apocryphasecurity.com>
2024-02-21 19:55:41 +0000

    Formatting pass

--- codex/views/data/discord.py
+++ codex/views/data/discord.py
@@ -9,11 +9,12 @@ from codex.serialisers.characters import CharacterDetailsSerialiser
 
 
 class DiscordAPIPermissions(permissions.BasePermission):
-    """ Check permissions for queries to this endpoint """
+    """Check permissions for queries to this endpoint"""
+
     def has_permission(self, request, view):
         try:
-            key = request.data.get('apikey')
-            assert(key is not None)
+            key = request.data.get("apikey")
+            assert key is not None
             if APIKey.objects.filter(value=key).exists():
                 return True
             else:
@@ -23,16 +24,17 @@ class DiscordAPIPermissions(permissions.BasePermission):
 
 
 class DiscordBotQueryView(APIView):
-    """ Endpoint for discord bots to make queries by discord ID """
+    """Endpoint for discord bots to make queries by discord ID"""
+
     permission_classes = [DiscordAPIPermissions]
 
     def post(self, request, query_type):
-        """ request to be sent as a post containing APIKey and Discord ID """
-        discord_id = request.data.get('discord_id')
-        if query_type == 'character':
+        """request to be sent as a post containing APIKey and Discord ID"""
+        discord_id = request.data.get("discord_id")
+        if query_type == "character":
             queryset = Character.objects.filter(player__discord_id__iexact=discord_id)
             queryset = queryset.filter(public=True)
 
             serialiser = CharacterDetailsSerialiser(queryset, many=True)
             return Response(serialiser.data, HTTP_200_OK)
-        return Response({'message': 'Unknown query type, valid are /character/ and /items/'}, HTTP_400_BAD_REQUEST)
+        return Response({"message": "Unknown query type, valid are /character/ and /items/"}, HTTP_400_BAD_REQUEST)
