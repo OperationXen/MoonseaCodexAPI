cdcb4cac90704c75603c150dd24aeaf78cab76d0
OperationXen <operationxen@apocryphasecurity.com>
2024-08-10 00:32:46 +0100

    Added trusted origin (#167)

--- core/settings.py
+++ core/settings.py
@@ -14,6 +14,7 @@ if DOMAIN:
     DEBUG = LIVE_DEBUG
     ALLOWED_HOSTS = [DOMAIN]
     ADMIN_URL = "/moonseacodex/admin"
+    CSRF_TRUSTED_ORIGINS = [f"https://{DOMAIN}"]
 else:
     DEBUG = True
     ALLOWED_HOSTS = ["127.0.0.1"]
