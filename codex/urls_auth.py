12d1422422dc73cd40d1ef968350f1eb5c3748fa
OperationXen <OperationXen@apocryphasecurity.com>
2025-01-12 12:57:47 +0000

    Updated

--- codex/urls_auth.py
+++ codex/urls_auth.py
@@ -6,12 +6,12 @@ from codex.views.auth.reset import RequestPasswordReset, PasswordReset
 from codex.views.auth.details import UserDetailsView
 
 urlpatterns = [
-    re_path("^login/?", LoginCodexUser.as_view(), name="login"),
-    re_path("^logout/?", LogoutCodexUser.as_view(), name="logout"),
-    re_path("^register/?", RegisterCodexUser.as_view(), name="register"),
-    re_path("^change_password", ChangeCodexUserPassword.as_view(), name="change_password"),
-    re_path("^forgot_password", RequestPasswordReset.as_view(), name="forgot_password"),
-    re_path("^user_details", UserDetailsView.as_view(), name="user_details"),
-    re_path("^password_reset/?", PasswordReset.as_view(), name='password_reset'),
-    re_path("^activate/(?P<user_id>[0-9]+)/(?P<token>[\w\-]+)/", ActivateCodexUser.as_view(), name="activate"),
+    re_path(r"^login/?", LoginCodexUser.as_view(), name="login"),
+    re_path(r"^logout/?", LogoutCodexUser.as_view(), name="logout"),
+    re_path(r"^register/?", RegisterCodexUser.as_view(), name="register"),
+    re_path(r"^change_password", ChangeCodexUserPassword.as_view(), name="change_password"),
+    re_path(r"^forgot_password", RequestPasswordReset.as_view(), name="forgot_password"),
+    re_path(r"^user_details", UserDetailsView.as_view(), name="user_details"),
+    re_path(r"^password_reset/?", PasswordReset.as_view(), name="password_reset"),
+    re_path(r"^activate/(?P<user_id>[0-9]+)/(?P<token>[\w\-]+)/", ActivateCodexUser.as_view(), name="activate"),
 ]
--- discord_auth/urls.py
+++ discord_auth/urls.py
@@ -4,8 +4,8 @@ from discord_auth.views import discord_login, discord_auth_done, discord_auth_co
 
 
 urlpatterns = [
-    re_path("login/?", discord_login, name="discord_login"),
-    re_path("done/?", discord_auth_done, name="discord_auth_done"),
-    re_path("complete/?", discord_auth_complete, name="discord_auth_complete"),
-    re_path("failed/?", discord_auth_failed, name="discord_auth_failed"),
+    re_path(r"login/?", discord_login, name="discord_login"),
+    re_path(r"done/?", discord_auth_done, name="discord_auth_done"),
+    re_path(r"complete/?", discord_auth_complete, name="discord_auth_complete"),
+    re_path(r"failed/?", discord_auth_failed, name="discord_auth_failed"),
 ]
