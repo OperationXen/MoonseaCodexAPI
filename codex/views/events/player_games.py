88a4591de62802b6a30383ce56cfb05205eaf2c5
OperationXen <OperationXen@apocryphasecurity.com>
2025-02-14 23:06:56 +0000

    removed unused imports

--- codex/views/events/player_games.py
+++ codex/views/events/player_games.py
@@ -3,9 +3,6 @@ from rest_framework.status import *
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
-from codex.models.events import Game
-from codex.models.character import Character
-
 from codex.serialisers.character_events import CharacterGameSerialiser
 from codex.serialisers.characters import CharacterSerialiser
 
