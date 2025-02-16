a79b8a8bd1a2a4e74d0a512442da26895d3969b8
OperationXen <OperationXen@apocryphasecurity.com>
2025-02-16 22:07:40 +0000

    Fixed bad fixtures

--- codex/tests/imports/test_parser.py
+++ codex/tests/imports/test_parser.py
@@ -7,9 +7,9 @@ class CSVImportParserTest(TestCase):
     """Tests for the import parser functions"""
 
     def test_parse_classes_single(self):
-        test_class_1 = {"class": "Wizard", "subclass": "Abjuration", "value": 10}
-        test_class_2 = {"class": "Fighter", "subclass": "", "value": 3}
-        test_class_3 = {"class": "Sorcerer", "subclass": "Draconic Red", "value": 15}
+        test_class_1 = {"name": "Wizard", "subclass": "Abjuration", "value": 10}
+        test_class_2 = {"name": "Fighter", "subclass": "", "value": 3}
+        test_class_3 = {"name": "Sorcerer", "subclass": "Draconic Red", "value": 15}
 
         self.assertEqual(parse_classes("Wizard (Abjuration) 10"), [test_class_1])
         self.assertEqual(parse_classes("Fighter - 3"), [test_class_2])
@@ -17,8 +17,8 @@ class CSVImportParserTest(TestCase):
         self.assertEqual(parse_classes("sorcerer (draconic - red) 15"), [test_class_3])
 
     def test_parse_classes_multiple(self):
-        test_class_1 = {"class": "Sorcerer", "subclass": "Wild Magic", "value": 5}
-        test_class_2 = {"class": "Paladin", "subclass": "", "value": 1}
+        test_class_1 = {"name": "Sorcerer", "subclass": "Wild Magic", "value": 5}
+        test_class_2 = {"name": "Paladin", "subclass": "", "value": 1}
 
         classes = parse_classes("Paladin / 5 Sorcerer (Wild magic)")
         self.assertEqual(classes[1], test_class_1)
