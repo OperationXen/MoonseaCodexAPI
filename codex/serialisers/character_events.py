7d07ecc46f4817a7829be2c9b3055a72c3b25d98
Daemoxia <operationxen@apocryphasecurity.com>
2025-03-16 19:30:34 +0000

    Added characters to game serialiser (#270)

--- codex/serialisers/character_events.py
+++ codex/serialisers/character_events.py
@@ -1,19 +1,29 @@
 from rest_framework import serializers
 
 from codex.serialisers.base import MoonseaCodexSerialiser
+from codex.models.character import Character
 from codex.models.events import Game
+
+
+class PartyCharacterSerialiser(serializers.ModelSerializer):
+    class Meta:
+        model = Character
+        read_only_fields = ["uuid", "name"]
+        fields = [*read_only_fields]
 
 
 class CharacterGameSerialiser(MoonseaCodexSerialiser):
     """serialiser for games played"""
 
     event_type = serializers.ReadOnlyField(default="game")
+    characters = PartyCharacterSerialiser(many=True, read_only=True)
 
     class Meta:
         model = Game
         read_only_fields = [
             "uuid",
             "editable",
+            "characters",
             "event_type",
         ]
         fields = [
