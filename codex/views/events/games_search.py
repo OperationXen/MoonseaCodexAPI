4cab0b074155ebbc8cfa18ae3ab3d4e08a5f8432
Daemoxia <operationxen@apocryphasecurity.com>
2025-04-18 22:40:40 +0100

    Added search for games by module and date (#292)

---
+++ codex/views/events/games_search.py
@@ -0,0 +1,22 @@
+from rest_framework.status import *
+from rest_framework.views import APIView, Response
+from rest_framework.permissions import IsAuthenticated
+
+from codex.serialisers.dm_events import DMGameSerialiser
+from codex.models.events import Game
+
+
+class SearchGamesView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        """Search across all games"""
+        module = request.data.get("module")
+        datetime = request.data.get("datetime")
+
+        try:
+            games = Game.objects.filter(module__iexact=module).filter(datetime__date=datetime[0:10])
+            serialised = DMGameSerialiser(games, many=True, context={"user": request.user})
+            return Response(serialised.data)
+        except Exception as e:
+            return Response({"message": "Error in search"}, HTTP_400_BAD_REQUEST)
