e3eb3698072bead8b4ac1f1972932a2458d2befe
OperationXen <OperationXen@apocryphasecurity.com>
2025-04-18 15:58:47 +0100

    Fixed weird broken test

--- codex/tests/views/events/test_views_events_dm_games.py
+++ codex/tests/views/events/test_views_events_dm_games.py
@@ -14,78 +14,81 @@ class TestDMGamesCRUDViews(TestCase):
 
     fixtures = ["test_users", "test_dungeonmaster_games"]
     valid_data = {
-        'name': 'Tier 1 Adventure Reward',
-        'dm_name': 'Test user',
-        'module': 'DDAL06-01 A thousand tiny deaths',
-        'location': 'Foundry',
-        'hours': 9,
-        'hours_notes': "4 hours + 4 hours mentoring + 1 hour for safety tools",
-        'notes': 'A nice clean run through with no character deaths at all',
-        
+        "name": "Tier 1 Adventure Reward",
+        "dm_name": "Test user",
+        "module": "DDAL06-01 A thousand tiny deaths",
+        "location": "Foundry",
+        "hours": 9,
+        "hours_notes": "4 hours + 4 hours mentoring + 1 hour for safety tools",
+        "notes": "A nice clean run through with no character deaths at all",
         "gold": 250,
         "downtime": 0,
         "levels": 1,
     }
 
-
     def test_anonymous_user_cannot_create_dm_rewards(self) -> None:
-        """A user who isn't logged in should be prevented from creating a dm reward """
+        """A user who isn't logged in should be prevented from creating a dm reward"""
         test_data = copy(self.valid_data)
 
         response = self.client.post(reverse("dm_game-list"), test_data)
         self.assertEqual(response.status_code, HTTP_403_FORBIDDEN)
 
     def test_user_can_log_dmed_game(self) -> None:
-        """ A user can record a game they DMed """
+        """A user can record a game they DMed"""
         test_data = copy(self.valid_data)
 
         self.client.login(username="testuser1", password="testpassword")
-        response = self.client.post(reverse('dm_game-list'), test_data)
+        response = self.client.post(reverse("dm_game-list"), test_data)
         self.assertEqual(response.status_code, HTTP_201_CREATED)
-        game = Game.objects.get(uuid=response.data['uuid'])
+        game = Game.objects.get(uuid=response.data["uuid"])
         self.assertIsInstance(game, Game)
-        self.assertEqual(game.notes, test_data['notes'])
+        self.assertEqual(game.notes, test_data["notes"])
 
     def test_user_service_hours_updated_on_game_log_creation(self) -> None:
-        """ Ensure a user's available dm hours are updated when a game is recorded """
+        """Ensure a user's available dm hours are updated when a game is recorded"""
         test_data = copy(self.valid_data)
 
         self.client.login(username="testuser1", password="testpassword")
-        initial_hours = DungeonMasterInfo.objects.get(player__username='testuser1').hours
-        response = self.client.post(reverse('dm_game-list'), test_data)
-        current_hours = DungeonMasterInfo.objects.get(player__username='testuser1').hours
-        self.assertEqual(current_hours, initial_hours + test_data['hours'])
+        initial_hours = DungeonMasterInfo.objects.get(player__username="testuser1").hours
+        response = self.client.post(reverse("dm_game-list"), test_data)
+        current_hours = DungeonMasterInfo.objects.get(player__username="testuser1").hours
+        self.assertEqual(current_hours, initial_hours + test_data["hours"])
 
     def test_user_can_delete_own_dmed_games(self) -> None:
         self.client.login(username="testuser1", password="testpassword")
-        
+
         game = Game.objects.get(pk=1)
         self.assertIsInstance(game, Game)
-        response = self.client.delete(reverse('dm_game-detail', kwargs={'pk': game.uuid}))
+        self.assertEqual(game.owner.username, "testuser1")
+        response = self.client.delete(reverse("dm_game-detail", kwargs={"pk": game.uuid}))
         self.assertEqual(response.status_code, HTTP_200_OK)
         with self.assertRaises(Game.DoesNotExist):
             game = Game.objects.get(pk=1)
 
     def test_user_can_update_own_dmed_games(self) -> None:
         self.client.login(username="testuser1", password="testpassword")
-        test_data = { 'gold': 1337}
-        
+        test_data = {"gold": 1337}
+
         game = Game.objects.get(pk=1)
         self.assertIsInstance(game, Game)
         self.assertEqual(game.gold, 100)
-        response = self.client.patch(reverse('dm_game-detail', kwargs={'pk': game.uuid}), json.dumps(test_data), content_type="application/json")
+        response = self.client.patch(
+            reverse("dm_game-detail", kwargs={"pk": game.uuid}), json.dumps(test_data), content_type="application/json"
+        )
         self.assertEqual(response.status_code, HTTP_200_OK)
         game.refresh_from_db()
-        self.assertEqual(game.gold, test_data['gold'])
+        self.assertEqual(game.gold, test_data["gold"])
 
     def test_user_cannot_update_other_dmed_games(self) -> None:
         self.client.login(username="testuser2", password="testpassword")
-        test_data = { 'gold': 1337}
-        
+        test_data = {"gold": 1337}
+
         game = Game.objects.get(pk=1)
         self.assertIsInstance(game, Game)
         self.assertEqual(game.gold, 100)
-        response = self.client.patch(reverse('dm_game-detail', kwargs={'pk': game.uuid}), json.dumps(test_data), content_type="application/json")
+        response = self.client.patch(
+            reverse("dm_game-detail", kwargs={"pk": game.uuid}), json.dumps(test_data), content_type="application/json"
+        )
         self.assertEqual(response.status_code, HTTP_403_FORBIDDEN)
         game.refresh_from_db()
         self.assertEqual(game.gold, 100)
@@ -93,16 +96,16 @@ class TestDMGamesCRUDViews(TestCase):
     def test_list_own_by_default(self) -> None:
         self.client.login(username="testuser1", password="testpassword")
 
-        response = self.client.get(reverse('dm_game-list'))
+        response = self.client.get(reverse("dm_game-list"))
         self.assertEqual(response.status_code, HTTP_200_OK)
-        self.assertEqual(response.data['count'], 1)
-        self.assertEqual(response.data['results'][0]['name'], "Lost Tales of Myth Drannor")
+        self.assertEqual(response.data["count"], 1)
+        self.assertEqual(response.data["results"][0]["name"], "Lost Tales of Myth Drannor")
 
     def test_can_list_games_by_dm_uuid(self) -> None:
-        """ a request for games should be filtered by the DM identifier """
-        dm_uuid = DungeonMasterInfo.objects.get(player__username='testuser1').uuid
+        """a request for games should be filtered by the DM identifier"""
+        dm_uuid = DungeonMasterInfo.objects.get(player__username="testuser1").uuid
 
-        response = self.client.get(reverse('dm_game-list') + f"?dm={dm_uuid}")
+        response = self.client.get(reverse("dm_game-list") + f"?dm={dm_uuid}")
         self.assertEqual(response.status_code, HTTP_200_OK)
-        self.assertEqual(response.data['count'], 1)
-        self.assertEqual(response.data['results'][0]['name'], "Lost Tales of Myth Drannor")
+        self.assertEqual(response.data["count"], 1)
+        self.assertEqual(response.data["results"][0]["name"], "Lost Tales of Myth Drannor")
