119373d726043aea128a62182477afe37702af59
OperationXen <OperationXen@apocryphasecurity.com>
2025-05-04 14:31:41 +0100

    Added fields to character

--- codex/models/character.py
+++ codex/models/character.py
@@ -5,19 +5,22 @@ from django.db import models
 
 from .users import CodexUser
 
+
 def get_user_artwork_path(instance, filename):
-    """ Returns a path for image storage on a per user basis """
-    rand_string = ''.join(random.choices(string.ascii_letters, k = 8))
+    """Returns a path for image storage on a per user basis"""
+    rand_string = "".join(random.choices(string.ascii_letters, k=8))
     return f"{instance.player.pk}/artwork/{instance.name}/{rand_string}-{filename}"
 
+
 def get_user_token_path(instance, filename):
-    """ Returns a path for image storage on a per user basis """
-    rand_string = ''.join(random.choices(string.ascii_letters, k = 8))
+    """Returns a path for image storage on a per user basis"""
+    rand_string = "".join(random.choices(string.ascii_letters, k=8))
     return f"{instance.player.pk}/tokens/{instance.name}/{rand_string}-{filename}"
 
 
 class Character(models.Model):
     """Representation of a character"""
+
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
     player = models.ForeignKey(
         CodexUser,
@@ -29,34 +32,47 @@ class Character(models.Model):
     name = models.CharField(max_length=256, blank=False, default="Unnamed character")
     artwork = models.ImageField(upload_to=get_user_artwork_path, null=True, blank=True)
     token = models.ImageField(upload_to=get_user_token_path, null=True, blank=True)
-    sheet = models.URLField(max_length=256, blank=True, null=True, help_text="Link to DND Beyond character sheet")
     public = models.BooleanField(default=True, help_text="Allow anyone to view this character")
-    season = models.CharField(max_length=128, blank=True, null=True, default="11", help_text="AL season that this character was created")
     # More detailed information about the character
     race = models.CharField(max_length=128, blank=True, null=True)
-    level = models.IntegerField(default=1, help_text='Total level')
+    level = models.IntegerField(default=1, help_text="Total level")
     classes = models.JSONField(default=dict, help_text="A JSON object detailing current classes")
     gold = models.FloatField(null=True, blank=True, help_text="Gold held by character (silver and copper in decimal)")
     downtime = models.FloatField(null=True, blank=True, help_text="Days of downtime")
+    # optional fields
+    campaign = models.CharField(max_length=128, default="Forgotten Realms", blank=True, null=True)
+    bastion = models.CharField(max_length=256, blank=True, null=True, help_text="Link to bastion map")
+    faction = models.CharField(max_length=128, blank=True, null=True, help_text="Faction and reknown level")
+    sheet = models.URLField(max_length=256, blank=True, null=True, help_text="Link to DND Beyond character sheet")
+    season = models.CharField(
+        max_length=128, blank=True, null=True, default="11", help_text="AL season that this character was created"
+    )
     # Useful information
-    ac = models.IntegerField(null=True, verbose_name='AC', help_text='Base armour class')
-    hp = models.IntegerField(null=True, verbose_name='HP', help_text='Max HP')
-    pp = models.IntegerField(null=True, verbose_name='PP', help_text='Passive perception')
-    dc = models.IntegerField(null=True, verbose_name='DC', help_text='Spell Save DC')
-    init = models.IntegerField(null=True, verbose_name='Initiative', help_text='Character initiative modifier')
-    vision = models.CharField(max_length=128, blank=True, null=True, help_text='Any special vision modes (eg darkvision)')
+    ac = models.IntegerField(null=True, verbose_name="AC", help_text="Base armour class")
+    hp = models.IntegerField(null=True, verbose_name="HP", help_text="Max HP")
+    pp = models.IntegerField(null=True, verbose_name="PP", help_text="Passive perception")
+    dc = models.IntegerField(null=True, verbose_name="DC", help_text="Spell Save DC")
+    init = models.IntegerField(null=True, verbose_name="Initiative", help_text="Character initiative modifier")
+    vision = models.CharField(
+        max_length=128, blank=True, null=True, help_text="Any special vision modes (eg darkvision)"
+    )
     # Biographical information
-    biography = models.TextField(blank=True, null=True, help_text='Character biography')
-    dm_text = models.TextField(blank=True, null=True, verbose_name='DM help text', help_text='Any information that may be useful for your DM to know ahead of time')
+    biography = models.TextField(blank=True, null=True, help_text="Character biography")
+    dm_text = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="DM help text",
+        help_text="Any information that may be useful for your DM to know ahead of time",
+    )
 
     def __str__(self):
-        """ String representation """
+        """String representation"""
         if self.player:
             return f"{self.player.username} - {self.name}"
         return f"[Orphaned] - {self.name}"
 
     def save(self, *args, **kwargs):
-        """ Override save method to auto calculate level from class levels """
+        """Override save method to auto calculate level from class levels"""
         # coerce classes to a list if given a single instance
         if type(self.classes) == dict:
             self.classes = [self.classes]
@@ -64,7 +80,7 @@ class Character(models.Model):
         accumulator = 0
         try:
             for item in self.classes:
-                accumulator += item['value']
+                accumulator += item["value"]
             self.level = accumulator
         except Exception:
             self.level = 1
